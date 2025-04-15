fab6e960896519f62b5cc030bd048c2c3e99747b
mariachernaya <5chernaya5.5@gmail.com>
2025-04-15 10:38:10 +0300

    Update login.php

--- zadanie_5/login.php
+++ zadanie_5/login.php
@@ -12,7 +12,13 @@
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST')
     {
-        require('database.php');
+        
+    $db;
+	$user = 'u68790'; 
+	$pass = '4247220'; 
+	$db = new PDO('mysql:host=localhost;dbname=u68790', $user, $pass,
+	[PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); 
+        
         $login = $_POST['login'];
         $password = md5($_POST['password']);
         try
