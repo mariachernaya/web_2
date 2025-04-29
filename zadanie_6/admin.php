e26d9dcbe0def493229973da758de89ab5bb4a81
mariachernaya <5chernaya5.5@gmail.com>
2025-04-29 12:28:55 +0300

    Update admin.php

--- zadanie_6/admin.php
+++ zadanie_6/admin.php
@@ -52,7 +52,7 @@ if (count($_POST)) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="bootstrap.min.css">
-  <link rel="stylesheet" href="admin.css">
+  <link rel="stylesheet" href="adm.css">
   <title>Задание_6_админка</title>
 </head>
 
